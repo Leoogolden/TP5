40f5670a39c706746ded8fad178b047084b9dc8f
Android Studio <devandroid@ort>
2019-07-04 12:11:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp5_goldenberg_wainberg/fragmentBuscar.java
@@ -0,0 +1,17 @@
+package com.example.tp5_goldenberg_wainberg;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmentBuscar extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+    }
+}
