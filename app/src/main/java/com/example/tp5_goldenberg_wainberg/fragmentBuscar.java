64dcc8bf28a92780d47b0020895de0b88eb21b21
Android Studio <devandroid@ort>
2019-07-16 10:36:57 -0300

    ahi va historia de juguete

--- app/src/main/java/com/example/tp5_goldenberg_wainberg/fragmentBuscar.java
+++ app/src/main/java/com/example/tp5_goldenberg_wainberg/fragmentBuscar.java
@@ -23,16 +23,24 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.acl.Group;
+import java.util.ArrayList;
+import java.util.List;
 
 public class fragmentBuscar extends Fragment {
     String textoAbuscar = new String();
     View vistadevuelve;
+    ArrayList<Peliculas> ListaResultados = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         vistadevuelve = inflater.inflate(R.layout.fragment_resultados, container, false);
         Bundle bundel = getArguments();
         textoAbuscar = bundel.getString("textobuscar");
+        Log.d("entra", "que onda");
+
+        Busqueda tarea = new Busqueda();
+        tarea.execute();
 
         return vistadevuelve;
     }
@@ -44,15 +52,15 @@ public class fragmentBuscar extends Fragment {
                 URL rutatlantica = new URL("http://www.omdbapi.com/?apikey=73b36931&s=" + textoAbuscar);
                 HttpURLConnection conexion = (HttpURLConnection) rutatlantica.openConnection();
                 if (conexion.getResponseCode() == 200) {
-                    Log.d("AccesoAPI3", "conexion ok");
+                    Log.d("AccesoAPI", "conexion ok");
                     InputStream cuerporesspuesta = conexion.getInputStream();
                     InputStreamReader lectorrespuesta = new InputStreamReader(cuerporesspuesta, "UTF-8");
-                    Log.d("AccesoAPI3", "conexioion ok seguimos");
+                    Log.d("AccesoAPI", "conexioion ok seguimos");
 
                     ProcessJSONLeido(lectorrespuesta);
-                    Log.d("AccesoAPI3", "conexion ok daaale");
+                    Log.d("AccesoAPI", "conexion ok daaale");
                 } else {
-                    Log.d("AccesoAPI3", "Error en la conexion");
+                    Log.d("AccesoAPI", "Error en la conexion");
                 }
                 conexion.disconnect();
 
@@ -69,9 +77,17 @@ public class fragmentBuscar extends Fragment {
         parseador = new JsonParser();
         JsonObject objsoncrudo;
         objsoncrudo = parseador.parse(a).getAsJsonObject();
+        JsonArray busqueda = objsoncrudo.get("Search").getAsJsonArray();
         for (int i = 0; i < objsoncrudo.size(); i++) {
-            
+            JsonObject unapelicula;
+            unapelicula = busqueda.get(i).getAsJsonObject();
+            Peliculas aux = new Peliculas();
+            aux.setId(unapelicula.get("imdbID").getAsString());
+            aux.setAño(unapelicula.get("Year").getAsInt());
+            aux.setTitulo(unapelicula.get("Title").getAsString());
+            aux.setPoster(unapelicula.get("Poster").getAsString());
+            ListaResultados.add(aux);
         }
-
+        Log.d("lolaso", ListaResultados.get(2).getTitulo());
     }
 }
